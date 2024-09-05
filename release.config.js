ee0c5a89e7ac4a41588ca20a19926bc58d5c5712
David Hernandez <davidh@tonder.io>
2024-09-05 13:08:34 -0500

    first commit

---
+++ release.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    branches: [
+      "main", 
+      "next",
+      { name: "release/*", channel: "rc", prerelease: true },
+      { name: "hotfix/*", channel: "hotfix", prerelease: true },
+      { name: "feature/*", channel: "beta", prerelease: true }
+    ],
+    plugins: [
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
+      "@semantic-release/changelog",
+      "@semantic-release/npm",
+      "@semantic-release/github",
+      "@semantic-release/git",
+      [
+        "@semantic-release/exec",
+        {
+          "prepareCmd": "npm install && npm run build"
+        }
+      ]
+    ],
+    tagFormat: "v${version}"
+  };
